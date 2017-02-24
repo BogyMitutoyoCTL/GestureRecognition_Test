c9ffe0e6317c313482954701f8a648b4c597f29d
Henrik <henrik.hauser@mitutoyo-ctl.de>
2017-02-24 10:25:51 +0100

    removes unnecessary commented-out-code

--- GestureRecognitionClass.py
+++ GestureRecognitionClass.py
@@ -45,16 +45,3 @@ class GestureRecognizer():
 
                 self.direction = dirX if dirX != "" else dirY
         return self.direction
-
-
-"""
-                else:
-                    thickness = int(np.sqrt(bufferSize / float(i + 1)) * 2.5)
-                    cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-
-        cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.65, (0, 0, 255), 3)
-        cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
-                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.35, (0, 0, 255), 1)
-"""
