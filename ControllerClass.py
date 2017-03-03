d3e934ca4a7ce60ef52711fb067bc356ea962195
andy <andy.meissner@mitutoyo-ctl.de>
2017-03-03 07:45:31 +0100

    apply codereview suggestions for revision 29710ceee8260b891dcdf0f4aa73967ba325358a

--- ControllerClass.py
+++ ControllerClass.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 import time
+import tkinter.messagebox as message
 import cv2
 from PIL import Image, ImageTk
 
@@ -10,31 +11,31 @@ from FrameDrawingClass import FrameDrawing
 from GestureRecognitionClass import GestureRecognizer
 from HandRecognitionClass import HandRecognizer
 
+default_camera = 0
 
 class Controller():
     def __init__(self):
         self.colorChangedEvent = Event()
         self.colorChangedEvent.append(self.onColorChanged)
         self.mainWindowRefresh = None
-        self.capture = cv2.VideoCapture(0)
+        self.camera = cv2.VideoCapture(default_camera)
         self.HandRecognizer = HandRecognizer()
         self.GestureRecognizer = GestureRecognizer()
         self.FrameDrawing = FrameDrawing()
-
         # UI-Elements:
-        self.tkinterWindow = None
+        self.window = None
         self.colorPickerButton = None
         self.imageContainer = None
 
         self.initUI()
 
     def initUI(self):
-        self.tkinterWindow = Tk()
-        self.tkinterWindow.bind('<Escape>', lambda e: self.tkinterWindow.quit())
+        self.window = Tk()
+        self.window.bind('<Escape>', lambda e: self.window.quit())
 
-        self.colorPickerButton = Button(self.tkinterWindow, text="set color range", command=self.button_changeColorRange)
+        self.colorPickerButton = Button(self.window, text="set color range", command=self.button_changeColorRange)
         self.colorPickerButton.pack()
-        self.imageContainer = Label(self.tkinterWindow)
+        self.imageContainer = Label(self.window)
         self.imageContainer.pack()
 
     def displayFrame(self, frame):
@@ -43,9 +44,7 @@ class Controller():
         self.imageContainer.imgtk = imgTk
         self.imageContainer.configure(image=imgTk)
 
-    def routine(self):
-        _, frame = self.capture.read()
-
+    def _process_frame(self, frame):
         hand = self.HandRecognizer.getHand(frame)
         if hand is not None:
             self.FrameDrawing.drawHand(frame, hand)
@@ -55,22 +54,27 @@ class Controller():
                 self.FrameDrawing.drawGesture(frame, gesture)
         else:
             self.FrameDrawing.putText(frame, "No hand detected")
+        return frame
 
-        self.displayFrame(frame)
-        self.mainWindowRefresh = self.imageContainer.after(10, self.routine)
-
-        print(time.time())
+    def process_frame_loop(self):
+        _, frame = self.camera.read()
+        if frame is not None:
+            frame = self._process_frame(frame)
+            self.displayFrame(frame)
+        else:
+            message.showerror("Error", "No frame found")
+        self.mainWindowRefresh = self.imageContainer.after(10, self.process_frame_loop)
 
     def button_changeColorRange(self):
-        ColorRangePicker(self.capture, self.colorChangedEvent)
+        ColorRangePicker(self.camera, self.colorChangedEvent)
         if self.mainWindowRefresh is not None:
             self.imageContainer.after_cancel(self.mainWindowRefresh)
 
     def onColorChanged(self, lowerColor, upperColor):
         self.HandRecognizer.filter.setColor(lowerColor, upperColor)
-        self.routine()
+        self.process_frame_loop()
 
 if __name__ == "__main__":
     controller = Controller()
-    controller.routine()
-    controller.tkinterWindow.mainloop()
+    controller.process_frame_loop()
+    controller.window.mainloop()
